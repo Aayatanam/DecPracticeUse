8685c7ddf6067de06b948f308d17a918ab6c8839
unknown <aayatanam1993@gmail.com>
2019-12-30 12:47:59 +0530

    updated utilpackage

--- src/main/java/com/pra/Util/ElementUtil.java
+++ src/main/java/com/pra/Util/ElementUtil.java
@@ -1,22 +1,16 @@
 package com.pra.Util;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ElementUtil {
 	public WebDriver driver;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public static WebElement getElement(WebDriver driver, By locator) {
+		WebElement element = driver.findElement(locator);
+		return element;
+
+	}
 
 }
